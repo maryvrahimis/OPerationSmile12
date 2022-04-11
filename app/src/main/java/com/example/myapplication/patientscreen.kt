d7397ca75f784569c825d0b8448a95924f14040c
Miguel <lugomiguel65@yahoo.com>
2022-04-11 18:51:36 -0400

    pulling info from database to patients page attempt 1

--- app/src/main/java/com/example/myapplication/patientscreen.kt
+++ app/src/main/java/com/example/myapplication/patientscreen.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
 import android.app.Activity
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -17,37 +19,85 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.operationsmile.ui.theme.BgColor
 import android.widget.TextView
 import java.text.SimpleDateFormat
+import io.realm.Realm
+import org.bson.types.ObjectId
+import com.example.myapplication.databinding.ActivityPatientscreenBinding
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 import java.util.*
 
 
 class patientscreen : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    private var _binding: ActivityPatientscreenBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patientscreen)
-
         val spinner: Spinner = findViewById(R.id.spinner)
         val adapter = ArrayAdapter.createFromResource(this, R.array.names_array, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
         spinner.onItemSelectedListener = this
-
         val textView: TextView = findViewById(R.id.dateAndTime)
         val simpleDateFormat = SimpleDateFormat("MM.dd.yyyy'at' HH:mm:ss z")
         val currentDateAndTime: String = simpleDateFormat.format(Date())
         textView.text = currentDateAndTime
+
+
+        val realm = Realm.getDefaultInstance()
+        Log.v(ContentValues.TAG, "Successfully opened a realm at: ${realm.path}")
+
+        //val patients = realm.where(Patients::class.java)
+        realm.executeTransaction { r: Realm ->
+            // Instantiate the class using the factory function.
+            val patients = r.createObject(Patients::class.java, ObjectId())
+            // Configure the instance.
+            patients.name = "this is a test"
+        }
+
+        val task2 = realm.where(Patients::class.java).equalTo("name","John Smith").findFirst()
+        if (task2 != null) {
+            println("THIS IS A TEST ${task2.name}")
+            // binding.textView6.text = "${task2.word}"
+        }
+        val opa = "${task2!!.name}"
+        realm.close()
+       binding.testName.text = opa
     }
+
+
 
     override fun onItemSelected(parent: AdapterView<*>? , view: View?, position: Int, id: Long) {
         val text = parent?.getItemIdAtPosition(position).toString()
-        Toast.makeText(getParent(), text, Toast.LENGTH_SHORT).show()
     }
-
     override fun onNothingSelected(parent: AdapterView<*>) {
         // Another interface callback
     }
 
-
 }
+
+open class Frog(var name: String = "",
+                var age: Int = 0,
+                var species: String? = null,
+                var owner: String? = null): RealmObject()
+
+open class Patients(
+    var _id: ObjectId? = null,
+    var name: String = "",
+    var age: String? = null,
+    var username: String? = null,
+    var password: String? = null,
+    val k_sounds_completed: Boolean? = null,
+    val p_sounds_completed: Boolean? = null,
+    val l_sounds_completed: Boolean? = null,
+    val t_sounds_completed: Boolean? = null,
+    var k_sounds_completed_time: String? = null,
+    var p_sounds_completed_time: String? = null,
+    var l_sounds_completed_time: String? = null,
+    var t_sounds_completed_time: String? = null): RealmObject()
+
 
 
 
