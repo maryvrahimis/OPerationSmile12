482596ad0df14dcc911ebd6477f8237fd1e769f8
Miguel <lugomiguel65@yahoo.com>
2022-04-11 15:34:22 -0400

    hello

---
+++ app/src/main/java/com/example/myapplication/emailupload.kt
@@ -0,0 +1,56 @@
+package com.example.myapplication
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+
+class emailupload : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emailupload)
+        var button: Button
+        var sendto: EditText
+        var subject: EditText
+        var body: EditText
+
+        sendto = findViewById(R.id.editText1);
+        subject = findViewById(R.id.editText2);
+        body = findViewById(R.id.editText3);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val emailsend = sendto.text.toString()
+                val emailsubject = subject.text.toString()
+                val emailbody = body.text.toString()
+
+                // define Intent object
+                // with action attribute as ACTION_SEND
+                val intent = Intent(Intent.ACTION_SEND)
+
+                // add three fiels to intent using putExtra function
+                intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(emailsend))
+                intent.putExtra(Intent.EXTRA_SUBJECT, emailsubject)
+                intent.putExtra(Intent.EXTRA_TEXT, emailbody)
+
+                // set type of intent
+                intent.type = "message/rfc822"
+
+                // startActivity with intent with chooser
+                // as Email client using createChooser function
+                startActivity(
+                    Intent
+                        .createChooser(
+                            intent,
+                            "Choose an Email client :"
+                        )
+                )
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/patientscreen.kt
@@ -0,0 +1,53 @@
+package com.example.myapplication
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.operationsmile.ui.theme.BgColor
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class patientscreen : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patientscreen)
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+        val adapter = ArrayAdapter.createFromResource(this, R.array.names_array, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = this
+
+        val textView: TextView = findViewById(R.id.dateAndTime)
+        val simpleDateFormat = SimpleDateFormat("MM.dd.yyyy'at' HH:mm:ss z")
+        val currentDateAndTime: String = simpleDateFormat.format(Date())
+        textView.text = currentDateAndTime
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>? , view: View?, position: Int, id: Long) {
+        val text = parent?.getItemIdAtPosition(position).toString()
+        Toast.makeText(getParent(), text, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
+    }
+
+
+}
+
+
+
