fa85655f4edf8cb93d3996c48995727d9463e800
Miguel <lugomiguel65@yahoo.com>
2022-04-26 22:53:02 -0400

    P Lessons video

---
+++ app/src/main/java/com/example/myapplication/p_lessons_video.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
+class p_lessons_video : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plessons_video)
+
+        val VideoView: VideoView = findViewById(R.id.videoView)
+        val videoPath: String = "android.resource://" + packageName + "/" + R.raw.p_sound_instructions
+        val Uri: Uri = Uri.parse(videoPath)
+        VideoView.setVideoURI(Uri)
+
+        val MediaController: MediaController = MediaController(this)
+        VideoView.setMediaController(MediaController)
+        MediaController.setAnchorView(VideoView)
+    }
+}
