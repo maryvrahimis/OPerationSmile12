78440b1295946f8b7bcfc02d7fdba89981ae6725
Miguel <lugomiguel65@yahoo.com>
2022-04-29 22:54:49 -0400

    P Lessons should be connected

--- app/src/main/java/com/example/myapplication/p_lessons_video.kt
+++ app/src/main/java/com/example/myapplication/p_lessons_video.kt
@@ -1,33 +1,31 @@
 package com.example.myapplication
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
 import android.widget.MediaController
-import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+
 
 class p_lessons_video : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_plessons_video)
+        //setContentView(R.layout.activity_plessons_video)
 
-        val VideoView: VideoView = findViewById(R.id.videoView)
-        val videoPath: String = "android.resource://" + packageName + "/" + R.raw.p_sound_front
-        val Uri: Uri = Uri.parse(videoPath)
-        VideoView.setVideoURI(Uri)
+       // val VideoView: VideoView = findViewById(R.id.videoView)
+       // val videoPath: String = "android.resource://" + packageName + "/" + R.raw.p_sound_front
+       // val Uri: Uri = Uri.parse(videoPath)
+       // VideoView.setVideoURI(Uri)
 
         val MediaController: MediaController = MediaController(this)
-        VideoView.setMediaController(MediaController)
-        MediaController.setAnchorView(VideoView)
+       // VideoView.setMediaController(MediaController)
+        //MediaController.setAnchorView(VideoView)
 
 
-        val skip: Button = findViewById(R.id.skip_to_lessons)
+        /*
         skip.setOnClickListener(){
             val intent = Intent(this, LasSilabasFragment::class.java)
             startActivity(intent)
         }
+        */
 
 
     }
